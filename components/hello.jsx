192271e85ef860feed0b51011172d34bac145af4
Brandon Bruno <bmbruno@gmail.com>
2022-04-19 22:02:41 -0400

    Added additional component content.

--- components/hello.jsx
+++ components/hello.jsx
@@ -7,7 +7,12 @@ class HelloWorld extends React.Component {
     }
 
     render () {
-        return <h1>Hi</h1>;
+        return(
+            <>
+                <h1>Hello, World!</h1>
+                <p>Welcome to your first React application.</p>
+            </>
+        );
     }
 }
 
