eab8045f17111ec578c3358bbbcfa700db0e1713
Brandon Bruno <bmbruno@gmail.com>
2022-04-20 15:11:10 -0400

    Added date/time output to HelloWorld component.

--- components/HelloWorld.jsx
+++ components/HelloWorld.jsx
@@ -5,12 +5,28 @@ class HelloWorld extends React.Component {
 
     constructor() {
         super();
+
+        this.state = {
+            today: new Date()
+        }
+
+    }
+
+    formatDate(date) {
+
+        let day = date.getDate();
+        let month = date.getMonth() + 1;
+        let year = date.getFullYear();
+
+        return `${month}/${day}/${year}`;
+
     }
 
     render () {
         return(
             <>
                 <h1>Hello, World!</h1>
+                <p>Today's date is {this.formatDate(this.state.today)}</p>
                 <p>Welcome to your first React application.</p>
 
                 <ReactLinks openNewWindow="true" />
