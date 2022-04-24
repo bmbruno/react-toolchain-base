6a23e23cab098e487c053a6c174876db2c173965
Brandon Bruno <bmbruno@gmail.com>
2022-04-24 07:48:40 -0400

    Fixed typo with 'ReactDom'

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import HelloWorld from '/components/HelloWorld.jsx'
 
-// This uses the React 18 'createRoot' method in place of ReactDom.render() from React 17 and earlier
+// This uses the React 18 'createRoot' method in place of ReactDOM.render() from React 17 and earlier
 const container = document.getElementById('app');
 const root = createRoot(container);
 root.render( <HelloWorld/> );
